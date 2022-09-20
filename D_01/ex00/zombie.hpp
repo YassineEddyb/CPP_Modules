42b8ba926042592a588c68bd3b637789d3de0440
yed-dyb <yassineeddyb@gmail.com>
2022-09-20 17:32:35 +0100

    D_01

---
+++ D_01/ex00/zombie.hpp
@@ -0,0 +1,15 @@
+# ifndef ZOMBIE_H
+# define ZOMBIE_H
+
+#include <iostream>
+#include <string>
+
+class Zombie {
+    private:
+        std::string name;
+    public: 
+        void anaunce(void);
+
+};
+
+# endif
